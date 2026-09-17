566b24f6d02f047782b8d448355b5bc6c799722b
UgwuRhema <rhemauros@protonmail.com>
2026-09-17 10:20:37 +0000

    will this even work?

---
+++ boot/stage2.asm
@@ -0,0 +1,15 @@
+bits 32
+org 0x8000
+
+global _start
+_start:
+	cli
+	mov byte [0xB8000], 'S'
+	mov byte [0xB8001], 0x07
+	mov byte [0xB8002], '2'
+	mov byte [0xB8003], 'M'
+
+.hang:
+	cli
+	hlt
+	jmp .hang
