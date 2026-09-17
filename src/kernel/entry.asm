37572c3baee11950b2d21cb8c1d2deb39e7fb9bf
UgwuRhema <rhemauros@protonmail.com>
2026-09-17 17:10:35 +0000

    Okay im starting to go over board, let me process things first

---
+++ src/kernel/entry.asm
@@ -0,0 +1,24 @@
+bits 64
+org 0x0
+
+section .text
+
+extern __bss_start, __bss_end
+extern kmain
+
+global _begin
+_begin:
+	cld ;clear the descriptor flag...
+	mov ax, 0x10
+	mov ds, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+	mov ss, ax
+
+	call kmain
+;infinite loop!
+;.hang:
+;	cli
+;	hlt
+;	jmp .hang
