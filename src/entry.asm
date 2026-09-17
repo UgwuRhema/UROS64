3d70067ce5e12f7b1e466774ad9946ca746ea7fb
UgwuRhema <rhemauros@protonmail.com>
2026-09-17 16:23:36 +0000

    Created a minimal entry that's prints text

---
+++ src/entry.asm
@@ -0,0 +1,26 @@
+bits 64
+org 0x0
+
+section .text
+
+global _begin
+_begin:
+	cld ;clear the descriptor flag...
+	mov ax, 0x10
+	mov ds, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+	mov ss, ax
+
+	;let's print something to verfiy successful jump
+	mov byte [0xb8000], 'K'
+	mov byte [0xb8001], 0x5f
+	mov byte [0xb8002], 'M'
+	mov byte [0xb8003], 0x5f
+
+;infinite loop!
+.hang:
+	cli
+	hlt
+	jmp .hang
