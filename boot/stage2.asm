84a4750ef0234e31e520828e51dceb3e67e58ec4
UgwuRhema <rhemauros@protonmail.com>
2026-09-17 11:33:28 +0000

    Okay time to read more sectors

--- boot/stage2.asm
+++ boot/stage2.asm
@@ -5,9 +5,9 @@ global _start
 _start:
 	cli
 	mov byte [0xB8000], 'S'
-	mov byte [0xB8001], 0x07
+	mov byte [0xB8001], 0x4D
 	mov byte [0xB8002], '2'
-	mov byte [0xB8003], 'M'
+	mov byte [0xB8003], 0x4D
 
 .hang:
 	cli
