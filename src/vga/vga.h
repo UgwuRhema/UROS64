ee5e94f1d29185ab78c6c36d35803800873a9f45
UgwuRhema <rhemauros@protonmail.com>
2026-09-18 20:24:08 +0000

    Alrighty, found yellow and that's all for today, gonna go study

--- src/vga/vga.h
+++ src/vga/vga.h
@@ -9,7 +9,7 @@ typedef uint16_t u16;
 /* my attributes UROS is only allowed to use */
 static const u16 WHITE = 0x07; /* for some reason i dont wann use #define MACROS */
 static const u16 BLUE = 0x01;
-static const u16 YELLOW = 0x4D;
+static const u16 YELLOW = 0x0e;
 static const u16 PURPLE = 0x05;
 
 static long unsigned int cursor_pos = 0;
