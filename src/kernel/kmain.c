f80141599050def816a9d5d1c79c32a69d88a134
UgwuRhema <rhemauros@protonmail.com>
2026-09-20 15:18:40 +0000

    Oh well

--- src/kernel/kmain.c
+++ src/kernel/kmain.c
@@ -2,19 +2,12 @@
 #include <stdint.h>
 #include "../vga/vga.h"
 
-static void recurse(void)
-{
-	void (* volatile rec)(void) = &recurse; /* correct no? */
-	rec();
-}
-
 void 
 kmain()
 {
 	kprint("Loading Kernel...\n" , WHITE);
 	kprint("UROS(Unrestricted Runtime Operating System) KERNEL ", WHITE);
 	kprint("v0.01\n", GREEN);
-	recurse(); /* this should cause a stack overflow then a page fault which will lead to a triple fault*/	
 	/* infinte loop */
 	while (1)
 	{
