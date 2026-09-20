e16a64289afe3a6dcdddeaa7e6aee2c4ba792ba8
UgwuRhema <rhemauros@protonmail.com>
2026-09-20 15:23:01 +0000

    time to add the helper functions

--- src/kernel/kmain.c
+++ src/kernel/kmain.c
@@ -1,6 +1,7 @@
 #include <stddef.h>
 #include <stdint.h>
 #include "../vga/vga.h"
+#include "../func/func.h"
 
 void 
 kmain()
@@ -8,7 +9,7 @@ kmain()
 	kprint("Loading Kernel...\n" , WHITE);
 	kprint("UROS(Unrestricted Runtime Operating System) KERNEL ", WHITE);
 	kprint("v0.01\n", GREEN);
-	kprint("Initializing Components and Necessities. \n", GREEN);
+	kprint("Initializing Components and Necessities... \n", GREEN);
 	/* infinte loop */
 	while (1)
 	{
