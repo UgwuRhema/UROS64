cdeed7d112dcc3cba9e05fb18a8f06276b6ca18e
UgwuRhema <hikarialiyev@gmail.com>
2026-09-21 15:29:10 +0000

    Starting to fix subtle bugs

--- src/kernel/kmain.c
+++ src/kernel/kmain.c
@@ -1,7 +1,9 @@
 #include <stddef.h>
 #include <stdint.h>
+
 #include "../vga/vga.h"
 #include "../func/func.h"
+#include "../ints/interrupts.h"
 
 void 
 kmain()
@@ -10,7 +12,11 @@ kmain()
 	kprint("UROS(Unrestricted Runtime Operating System) KERNEL ", WHITE);
 	kprint("v0.01\n", GREEN);
 	kprint("Initializing Components and Necessities... \n", GREEN);
-	/* infinte loop */
+    idt_init();
+    int a = 10;
+    int b = 0;
+    int c = a / b;
+    /* infinte loop */
 	while (1)
 	{
 		__asm__("hlt");
