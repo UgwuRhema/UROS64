f279905bcc6a37b3c71274438bde22b8df86ca77
UgwuRhema <hikarialiyev@gmail.com>
2026-09-21 15:15:53 +0000

    Just one handler let's test

---
+++ src/ints/handlers.c
@@ -0,0 +1,11 @@
+#include "handlers.h"
+
+__attribute__((interrupt)) void divide_by_zero(void *frame)
+{
+    kprint("KERNEL PANIC!\n", PURPLE);
+    kprint("Fatal exception in interrupt!\n", YELLOW);
+    kprint("Interrupt divide by zero\n", WHITE);
+    kprint("System Halted. Please reboot\n", WHITE);
+    while (1)
+        __asm__ volatile ("hlt");    
+}
---
+++ src/ints/handlers.h
@@ -0,0 +1,3 @@
+#include "../vga/vga.h"
+
+extern __attribute__((interrupt)) void divide_by_zero(void *);
