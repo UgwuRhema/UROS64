95be5d08c66da9e9727e9c3ba2a2985ca667284d
UgwuRhema <hikarialiyev@gmail.com>
2026-09-21 15:05:48 +0000

    Worked up some minimal fizes

--- src/ints/interrupts.c
+++ src/ints/interrupts.c
@@ -6,7 +6,7 @@ idt_set_entry(int index, uint8_t ist,uint64_t hand_addr, uint16_t selector, uint
    idt[index].offset_low = hand_addr & 0xffff; /* low 16 bits */
    idt[index].offset_middle = (hand_addr >> 16) & 0xffff; /* blah blah */
    idt[index].offset_high = (hand_addr >> 32);
-   idt[index].ist = ist;
+   idt[index].ist = ist & 0x07;
    idt[index].selector = selector;
    idt[index].zero = (uint8_t)0x0; /* i know this is pointless haha */
    idt[index].type_attr = attr; /* 0x8e for active and 0x0e for inactive */
@@ -16,9 +16,9 @@ void
 idt_load(void)
 {
     static struct IDTR idtr;
-    idtr.size = (sizeof(struct IDT_Entry) * _IDT_ENTRIES) - 1; /* if you look closely this is the same thing we did for the GDT*/
+    idtr.size = (uint16_t)(sizeof(idt) - 1); /* if you look closely this is the same thing we did for the GDT*/
     /* it was "dw gdt_end - gdt_start - 1" to get the size of the gdt */
-    idtr.address = (uint64_t)&idt;
+    idtr.address = (uint64_t)&idt[0];
 
     /* the assembly command... */
     __asm__ volatile ("lidt %0" : : "m"(idtr));
