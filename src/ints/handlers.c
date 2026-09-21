14dbf1e9f5d07bc1dc6a767cec21fe6a682a1fdb
UgwuRhema <hikarialiyev@gmail.com>
2026-09-21 16:37:47 +0000

    My clear screen is mediocre

--- src/ints/handlers.c
+++ src/ints/handlers.c
@@ -4,9 +4,9 @@ __attribute__((interrupt)) void divide_by_zero(void *frame)
 {
     clear_screen();
     kprint("KERNEL PANIC!\n", PURPLE);
-    kprint("Fatal exception in interrupt!\n", YELLOW);
-    kprint("Interrupt divide by zero\n", WHITE);
-    kprint("System Halted. Please reboot\n", WHITE);
+    kprint("Fatal exception in interrupt!\n", PURPLE);
+    kprint("Interrupt #0: Divide by zero\n", WHITE);
+    kprint("System Halted. Please reboot\n", BLUE);
     while (1)
         __asm__ volatile ("hlt");    
 }
