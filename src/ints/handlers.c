fd6dec5111df7f3d8e9b053e6f207a38131a8a5a
UgwuRhema <hikarialiyev@gmail.com>
2026-09-21 16:35:47 +0000

    Had to call it...

--- src/ints/handlers.c
+++ src/ints/handlers.c
@@ -2,6 +2,7 @@
 
 __attribute__((interrupt)) void divide_by_zero(void *frame)
 {
+    clear_screen();
     kprint("KERNEL PANIC!\n", PURPLE);
     kprint("Fatal exception in interrupt!\n", YELLOW);
     kprint("Interrupt divide by zero\n", WHITE);
