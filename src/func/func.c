96a3792766dec981621cfbc89feb1e05b11b7246
UgwuRhema <hikarialiyev@gmail.com>
2026-09-21 13:58:12 +0000

    Fixed the inb function

--- src/func/func.c
+++ src/func/func.c
@@ -1,7 +1,17 @@
 #include "func.h"
 
+//send commands to port...
 void 
 outb(u16 port, u8 val)
 {
     __asm__ volatile ("outb %0, %1" : : "a"(val), "Nd"(port));
+}
+
+//reads from port
+u8
+inb(u16 port)
+{
+	u8 val;
+	__asm__ volatile ("inb %1, %0" : "=a"(ret) : "Nd"(port));
+	return val;
 }
