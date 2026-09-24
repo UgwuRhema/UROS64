6b8eaed869f34149e60ae9bb1b50bbb9134a391e
UgwuRhema <hikarialiyev@gmail.com>
2026-09-24 08:58:06 +0000

    Why doesn't the trigger_pf_panic cause a kernel panic?!
    Signed-off-by: UgwuRhema <hikarialiyev@gmail.com>

--- src/shell/shell.c
+++ src/shell/shell.c
@@ -21,7 +21,10 @@ static void execute_command(const char *cmd)
         u8 good = 0x02;
         while (good & 0x02) good = inb(0x64);
         outb(0x64, 0xfe);
-    } else {
+    } else if (strcmp(cmd, "trigger_pf_panic") == 0){
+        volatile u64 *panic_ptr = (volatile u64 *)0xdeadbeefULL;
+        *panic_ptr = 0x1234;
+    }else {
         kprint("Unknown Command: ", PURPLE);
         kprint("'", WHITE);
         kprint(cmd, WHITE);
